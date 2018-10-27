124e017ae5e2c9c2460983cae0c58453b355739f
jdiazcano <terkitos@gmail.com>
2018-10-27 10:54:10 +0100

    Add jacoco and fatjar

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
+    jacoco
+    findbugs
     kotlin("jvm") version Versions.kotlin
 }
 
 group = "com.jdiazcano"
 version = "1.0-SNAPSHOT"
+val mainClass = ""
 
 repositories {
     mavenCentral()
@@ -30,4 +33,25 @@ java.sourceSets["test"].java {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+// Jacoco configuration
+tasks.withType<JacocoCoverageVerification> {
+    violationRules {
+        rule {
+            limit {
+                minimum = 0.8.toBigDecimal()
+            }
+        }
+    }
+}
+
+val fatJar = task("fatJar", type = Jar::class) {
+    baseName = "${project.name}-fat"
+    manifest {
+        attributes["Implementation-Version"] = version
+        attributes["Main-Class"] = mainClass
+    }
+    from(configurations.runtime.map { if (it.isDirectory) it else zipTree(it) })
+    with(tasks["jar"] as CopySpec)
 }
